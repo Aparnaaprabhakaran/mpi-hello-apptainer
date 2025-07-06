3b9857513d5eb6b777fdb80b18ebb8f28c862461
Aparna Prabhakaran <Aparna@meetings.flowthermolab.com>
2025-07-06 22:48:10 +0000

    Clean push: MPI Hello World app without SIF

---
+++ mpi_hello.c
@@ -0,0 +1,18 @@
+// mpi_hello.c
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    printf("Hello world from processor rank %d out of %d processors\n", world_rank, world_size);
+
+    MPI_Finalize();
+    return 0;
+}
